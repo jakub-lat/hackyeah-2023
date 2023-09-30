d7d744f0899012bfb0ba1bd8dfa19dc41fe8921c
Szymon <ikcilrep@outlook.com>
2023-09-30 22:41:35 +0200

    Assert key not null

--- src/lib/secrets.ts
+++ src/lib/secrets.ts
@@ -5,4 +5,4 @@ import { db } from "./firebase";
 const keysRef = doc(db, "keys", "oYSDpRcpmcjDNeE9ccHF") ;
 const docSnap = await getDoc(keysRef);
 
-export const openAiKey = docSnap.data()["value"];
+export const openAiKey = docSnap.data()!["value"];

77f02072408132ddbf6186398ac65417afc3ef62
PasiDisbanq <pasidisbanq@gmail.com>
2023-09-30 21:04:41 +0200

    Add StreetViewEmbed method

---
+++ src/components/street-view-embed.tsx
@@ -0,0 +1,14 @@
+const StreetViewEmbed = (src:string) : JSX.Element =>  {
+    return ( 
+        <iframe
+            src={src}
+            width="1000"
+            height="500"
+            style={{border:0}}
+            allowFullScreen={false}
+            referrerPolicy={"no-referrer-when-downgrade"}>
+        </iframe>   
+    );
+}
+ 
+export default StreetViewEmbed;

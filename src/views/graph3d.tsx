7022dce81f894228b51ed3f894b752fffe656cdc
Dawid Dieu <dawid.dieu@kalepa.com>
2023-09-30 18:59:39 +0200

    Update 3d graph

--- src/views/graph3d.tsx
+++ src/views/graph3d.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useRef, useState }  from 'react';
 import {Canvas, useThree} from 'react-three-fiber';
-import {OrbitControls} from '@react-three/drei';
+import {Billboard, OrbitControls, Text} from '@react-three/drei';
 import {Vector3} from 'three';
+import * as THREE from 'three';
+
 
 function SetupCamera() {
     const {camera} = useThree();
@@ -10,6 +12,27 @@ function SetupCamera() {
     camera.position.z = 10;
 
     return null; // This component doesn't render anything itself
+}
+
+
+const Dot = ({ label, onClick }: { label: string, onClick: (label: string) => void }) => {
+  const meshRef = useRef<THREE.Mesh>(null);
+  const [hovered, setHovered] = useState(false);
+
+  return (
+    <mesh
+      ref={meshRef}
+      onClick={() => onClick(label)}
+      onPointerOver={(e) => {
+        e.stopPropagation();  // This prevents the event from propagating further
+        setHovered(true);
+      }}
+      onPointerOut={() => setHovered(false)}
+    >
+      <sphereGeometry args={[0.1]} />
+      <meshStandardMaterial color={hovered ? 'yellow' : 'blue'} />
+    </mesh>
+  );
 }
 
 const Dots: React.FC<{ count: number }> = ({count}) => {
@@ -24,17 +47,31 @@ const Dots: React.FC<{ count: number }> = ({count}) => {
         return temp;
     }, [count]);
 
+    const handleDotClick = (label: string) => {
+        console.log(label);
+    }
+
     return (
         <group>
             {dots.map((dot, index) => (
-                <mesh key={index} position={dot}>
-                    <sphereGeometry args={[0.1]}/>
-                    <meshStandardMaterial color="blue"/>
-                </mesh>
+                <group key={index} position={[dot.x, dot.y, dot.z]}>
+                    <Dot label={"KIERUNEK"} onClick={handleDotClick} />
+                    <Billboard>
+                        <Text
+                            fontSize={0.2}
+                            position={[0, 0.2, 0]}
+                            anchorX="center"
+                            anchorY="middle"
+                        >
+                            {"SIEEMA"}
+                        </Text>
+                    </Billboard>
+                </group>
             ))}
         </group>
     );
 };
+
 
 const DotPlot3D: React.FC = () => {
     return (

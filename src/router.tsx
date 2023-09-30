200777e211a4a76abc125187234299a1c4735ca1
Jakub L <jakub@cubepotato.eu>
2023-09-30 14:54:14 +0200

    not found

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,13 @@
 import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
 import BaseLayout from "@/layouts/BaseLayout.tsx";
 import Home from "@/views/Home.tsx";
+import NotFound from "@/views/not-found.tsx";
 
 export default createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<BaseLayout />}>
             <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
         </Route>
     )
 );
---
+++ src/views/not-found.tsx
@@ -0,0 +1,6 @@
+export default function NotFound() {
+    return <div className={"p-10"}>
+        <h1 className={"text-3xl font-bold mb-3"}>404</h1>
+        <h1 className={"text-xl"}>Nie znaleziono strony</h1>
+    </div>
+}

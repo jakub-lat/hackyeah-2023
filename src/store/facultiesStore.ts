84117e883acbcd5922f3b70b98c8974d9f966a38
Dawid Dieu <dawid.dieu@gmail.com>
2023-10-01 01:32:18 +0200

    Update field of studies (#40)

--- src/store/facultiesStore.ts
+++ src/store/facultiesStore.ts
@@ -1,18 +1,23 @@
-import {firestore} from "../lib/firebase";
-import { collection, getDocs, query } from 'firebase/firestore';
+// import {firestore} from "../lib/firebase";
+// import { collection, getDocs, query } from 'firebase/firestore';
+import allFieldsOfStudy from "../data/allFieldsOfStudy.json";
 
 export interface IFaculty {
     id: string,
     type: string,
 }
 
-const getFaculties = async (): Promise<string[]> => {
-  const q = query(collection(firestore, 'faculties'));
-  const querySnapshot = await getDocs(q);
-  const data: IFaculty[] = querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id } as IFaculty));
-  // for each object in data return its id and name
-  const uniqueData = new Set(data.map(({ type }: any) => (type)));
-  return Array.from(uniqueData);
+// const getFaculties = async (): Promise<string[]> => {
+//   const q = query(collection(firestore, 'faculties'));
+//   const querySnapshot = await getDocs(q);
+//   const data: IFaculty[] = querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id } as IFaculty));
+//   // for each object in data return its id and name
+//   const uniqueData = new Set(data.map(({ type }: any) => (type)));
+//   return Array.from(uniqueData);
+// };
+
+const getFieldsOfStudy = () => {
+    return allFieldsOfStudy;
 };
 
-export default getFaculties;
+export default getFieldsOfStudy;

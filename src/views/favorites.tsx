dbb595145e428d913e83b4e4b0074cd373f281f3
TheMatiaz0 <mateusz@kusionowicz.com>
2023-10-01 10:46:08 +0200

    Add alert to favorites

--- src/views/favorites.tsx
+++ src/views/favorites.tsx
@@ -6,6 +6,9 @@ import {auth} from "@/lib/firebase.ts";
 import {useEffect} from "react";
 import {IUniversity, useUniStore} from "@/store/universityStore.ts";
 import universities from "@/data/universities.json";
+import {Alert, AlertDescription} from "@/components/ui/alert";
+import {AlertTitle} from "@/components/ui/alert.tsx";
+import {AlertCircle} from "lucide-react";
 
 export default function Favorites() {
     const {favorites, getFavorites} = useUniStore();
@@ -18,7 +21,16 @@ export default function Favorites() {
 
     const navigate = useNavigate();
     return <PageLayout className={"px-64"}>
-        <PageTitle className={"mt-4"}>Ulubione uczelnie/kierunki</PageTitle>
+        <PageTitle className={"mt-4 mb-4"}>Ulubione uczelnie/kierunki</PageTitle>
+
+        {selectedFields.length === 0 && <Alert className={'h-min w-[450px] border-red-600 border-opacity-50 bg-red-600/10'}>
+                <AlertCircle className="h-4 w-4" />
+                <AlertTitle>Uwaga!</AlertTitle>
+                <AlertDescription>
+                    Nie dodałeś jeszcze żadnych uczelni ani kierunków do Ulubionych.
+                </AlertDescription>
+            </Alert>}
+
         {/*<Filters />*/}
         <div className="flex flex-col lg:flex-row gap-5 mt-4">
             <div className="flex flex-col gap-3 w-full">

c72f8f7a449237a60372ee7177d6ccc97fd0605d
Jakub L <jakub@cubepotato.eu>
2023-10-01 02:48:32 +0200

    feat: university details

--- src/views/universities.tsx
+++ src/views/universities.tsx
@@ -1,30 +1,19 @@
 import PageLayout from "@/layouts/PageLayout.tsx";
 import Map from "@/components/ui/map.tsx";
 import UniMock from "@/mock/universities";
-import UniCard from "@/components/universities/uni-card";
-import { useState } from "react";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import Filters from "@/components/filters.tsx";
+import {useUniStore} from "@/store/universityStore.ts";
+import UniversityList from "@/components/university-list.tsx";
+import University from "@/components/university.tsx";
 
 export default function Universities() {
-    const [focus, setFocus] = useState(null)
+    // const [focus, setFocus] = useState(null)
+    const {focused, setFocused} = useUniStore();
 
     return <PageLayout>
         <Filters />
         <div className="flex flex-col lg:flex-row gap-5">
-            <ScrollArea className="lg:w-[35%] max-h-[80vh]">
-                <div className="flex flex-col gap-3">
-                    {UniMock.map((uni, i) =>
-                        <UniCard
-                            key={i}
-                            header={uni.name}
-                            description={`${uni.city}, kierunek: ${uni.major}`}
-                            icon={uni.icon}
-                            onClick={() => setFocus({ lat: uni.lat, lng: uni.lng })}
-                        />
-                    )}
-                </div>
-            </ScrollArea>
+            {focused ? <University university={focused} /> : <UniversityList />}
             <div className="lg:w-[65%] lg:h-[80vh]">
                 <Map
                     markers={UniMock.map((uni) => {
@@ -33,10 +22,10 @@ export default function Universities() {
                             lng: uni.lng,
                             badge: uni.name,
                             icon: uni.icon,
-                            onClick: () => setFocus({ lat: uni.lat, lng: uni.lng })
+                            onClick: () => setFocused(uni)
                         }
                     })}
-                    focus={focus}
+                    focus={focused}
                 />
             </div>
         </div>

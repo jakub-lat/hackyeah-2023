3915e6478ff502c0236c3289ba67f7cf8cde77a4
Szymon <ikcilrep@outlook.com>
2023-10-01 03:41:12 +0200

    Show actual universities on the list

--- src/views/universities.tsx
+++ src/views/universities.tsx
@@ -5,22 +5,62 @@ import UniCard from "@/components/universities/uni-card";
 import { useState } from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import Filters from "@/components/filters.tsx";
+import { useFilterStore } from "@/store/filterStore.ts";
+import universities from "../data/universities.json";
+import allFieldsOfStudy from "../data/fieldsOfStudy.json";
+/*
+    {
+      "name": "Uniwersytet Warszawski",
+      "city": "Warszawa",
+      "major": "Informatyka",
+      "lat": 52.2297,
+      "lng": 21.0122,
+      "icon": "https://promocja.strony.uw.edu.pl/wp-content/uploads/sites/339/2020/07/PL_zwykly_okragly.png"
+    },
+*/
+
+interface FieldOfStudy {
+    type: string;
+    universityId: string;
+    facultyName: string | null;
+    degree: Number;
+    numberOfPoints: Number;
+}
+
+interface University {
+    name: string;
+    longitude: Number;
+    latitude: Number;
+    city: string;
+    geoHash: string;
+}
 
 export default function Universities() {
     const [focus, setFocus] = useState(null)
+    const { selectedFields } = useFilterStore();
+    const selectedFieldsOfStudy = allFieldsOfStudy.filter((field) => selectedFields.includes(field.type));
+    const doesUniversityOfferAnySelectedField = (uni: University) => selectedFieldsOfStudy.map((f) => f.universityId).includes(uni.name);
+    const selectedUniversities = Array.from(new Set(universities.filter(doesUniversityOfferAnySelectedField)))
+    const getUniversityFieldsDescription = (uni: University) => {
+        const fields = new Set(selectedFieldsOfStudy.filter((f) => f.universityId === uni.name).map(f => f.type))
+        if (fields.size === 1) {
+            return `kierunek: ${Array.from(fields)[0]}`;
+        }
+        return `kierunki: ${Array.from(fields).join(", ")}`;
+    };
 
     return <PageLayout>
         <Filters />
         <div className="flex flex-col lg:flex-row gap-5">
             <ScrollArea className="lg:w-[35%] max-h-[80vh]">
                 <div className="flex flex-col gap-3">
-                    {UniMock.map((uni, i) =>
+                    {selectedUniversities.map((uni, i) =>
                         <UniCard
                             key={i}
                             header={uni.name}
-                            description={`${uni.city}, kierunek: ${uni.major}`}
-                            icon={uni.icon}
-                            onClick={() => setFocus({ lat: uni.lat, lng: uni.lng })}
+                            description={`${uni.city}, ${getUniversityFieldsDescription(uni)}`}
+                            icon={null}
+                            onClick={() => setFocus({ lat: uni.latitude, lng: uni.longitude })}
                         />
                     )}
                 </div>

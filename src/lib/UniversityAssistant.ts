a5239420b16f85e8d3d17bef67ff011c6d09760c
Szymon <ikcilrep@outlook.com>
2023-10-01 09:43:18 +0200

    Retry prompts if output cannot be parsed

--- src/lib/UniversityAssistant.ts
+++ src/lib/UniversityAssistant.ts
@@ -15,7 +15,7 @@ export default class UniversityAssistant {
             Zadawaj pytania tak, aby jak najszybciej uzupełnić te dane i nie zadawaj pytań, na które już masz odpowiedź. 
             suggestedFieldOfStudy wywnioskuj na podstawie zainteresowań użytkownika, nie pytaj wprost o kierunki studiów.
             Odpowiedź na pytania podawaj zawsze w formacie {"answer": ..., "isDataGathered": true/false, "possibleResponses": [...]/null}.
-            Pole possibleResponses uzupełniaj tylko wtedy, gdy Twoje pytanie ma mniej niż 4 możliwe odpowiedzi, niech każda odpowiedź będzie zwięzłym zdaniem w języku polskim.
+            Pole possibleResponses uzupełniaj tylko wtedy, gdy Twoje pytanie ma mniej niż 4 możliwe odpowiedzi, niech każda odpowiedź będzie zwięzłym zdaniem w języku polskim, pamiętaj o kropkach na ich końcu.
             Jeśli Twoje pytanie dotyczy konkretnego preferowanego miasta studiów, ustaw possibleResponses na null.
             Jeśli uzupełniłeś już wszystkie pola JSON, odpowiedz podając ten obiekt w polu answer odpowiedzi i ustawiając isDataGathered na true.
             W przeciwnym wypadku ustaw isDataGathered na false, a answer ustaw na tekst Twojej odpowiedzi.`;
@@ -23,11 +23,11 @@ export default class UniversityAssistant {
         this.messages.push({ role: 'assistant', content: 'Cześć! Opowiedziałbyś mi o tym co lubisz robić w wolnym czasie?' })
     }
 
-    async ask(prompt: string): Promise<AssistantAnswer> {
+    async ask(prompt: string, numberOfTries: number = 0): Promise<AssistantAnswer> {
         const newPrompt = `Pamiętaj, żeby odpowiedzieć w formacie 
         {"answer": ..., "isDataGathered": true/false, "possibleResponses": [...]/null} i dalej zbierać informacje do wypełnienia dokumentu JSON, 
         chyba że już wszystkie są zebrane. 
-        Jeśli possibleResponses nie jest nullem, niech będzie to lista możliwych odpowiedzi na Twoje pytanie zwięzłym zdaniem w języku polskim.
+        Jeśli possibleResponses nie jest nullem, niech będzie to lista możliwych odpowiedzi na Twoje pytanie zwięzłym zdaniem w języku polskim, pamiętaj o kropkach na ich końcu.
         Jeśli Twoje pytanie dotyczy konkretnego preferowanego miasta studiów, ustaw possibleResponses na null.
         Nie powtarzaj pytań, które zadałeś już wcześniej. Zdanie do odpowiedzi: ` + prompt;
         this.messages.push({ role: 'user', content: newPrompt });
@@ -45,12 +45,17 @@ export default class UniversityAssistant {
         this.messages.push(answer);
         try {
             return JSON.parse(answer.content!) as AssistantAnswer;
+
         } catch (e) {
-            return {
-                answer: answer.content!,
-                isDataGathered: false,
-                possibleResponses: null,
-            };
+            if (numberOfTries > 2) {
+                return {
+                    answer: answer.content!,
+                    isDataGathered: false,
+                    possibleResponses: null,
+                };
+            }
+            const promptAddition = "Pamiętaj, aby Twoją odpowiedź dało się sparsować jako JSON - nie dodawaj do niej nic co spowodowałoby błąd. ";
+            return this.ask(promptAddition + prompt, numberOfTries + 1);
         }
     }
 }
